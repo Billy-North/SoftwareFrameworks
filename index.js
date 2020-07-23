d30fee4142cbce8e72e63103a2ad4e1b6bf02a48
Billy-North <william.north@griffithuni.edu.au>
2020-07-23 20:38:36 +1000

    removed merge conflict

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 console.log("Hello World!")
-=======
+
 const addNums = (num1,num2) => {return num1 + num2}
 
 console.log("Hello World!")
 
 console.log(addNums(5,10))
->>>>>>> addingFunction
