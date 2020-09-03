bac1bedeb665986455cc9b45f761912df71307e6
Billy North <billynorth10@gmail.com>
2020-09-03 21:09:55 +1000

    added socket rooms

--- week6Lab/chat/server/socket.js
+++ week6Lab/chat/server/socket.js
@@ -1,10 +1,18 @@
 module.exports = {
     connect: function(io, PORT) {
+        let currentRoom;
         io.on('connection',(socket) => {
             console.log('User has connected on port ' + PORT + ' : ' + socket.id);
+
+            socket.on('room', function(room) {
+                currentRoom = room
+                socket.join(room);
+            });
+
             socket.on('message',(message) => {
-                io.emit('message',message)
+                io.to(currentRoom).emit('message',message);
             })
         })
     }
 }
+
